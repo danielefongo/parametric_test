3cab4eda9ffb639f54c634b0ab41fe69d182ce7a
danielefongo <danielefongo@gmail.com>
2023-02-02 12:08:44 +0100

    use module attributes

--- lib/parametric_test.ex
+++ lib/parametric_test.ex
@@ -1,8 +1,32 @@
 defmodule ParametricTest do
-  defmacro parametric_test(name, {:<-, _, [vars, params]}, context \\ %{}, do: body) do
-    Enum.map(params, fn param ->
+  defmacro parametric_test(name, params, do: body) do
+    setup_test(name, params, %{}, body)
+  end
+
+  defmacro parametric_test(name, params, context, do: body) do
+    setup_test(name, params, context, body)
+  end
+
+  def setup_test(name, {:<-, _, [vars, params = {:@, _, [{param, _, _}]}]}, context, body) do
+    quote do
+      Module.get_attribute(__MODULE__, unquote(param))
+      |> Enum.with_index()
+      |> Enum.map(fn {param, idx} ->
+        @parametric_test_idx idx
+        test "#{unquote(name)} #{inspect(idx + 1)}", unquote(context) do
+          unquote(vars) = Enum.at(unquote(params), @parametric_test_idx)
+          unquote(body)
+        end
+      end)
+    end
+  end
+
+  def setup_test(name, {:<-, _, [vars, params]}, context, body) do
+    params
+    |> Enum.with_index()
+    |> Enum.map(fn {param, idx} ->
       quote do
-        test "#{unquote(name)} #{inspect(unquote(param))}", unquote(context) do
+        test "#{unquote(name)} #{inspect(unquote(idx + 1))}", unquote(context) do
           unquote(vars) = unquote(param)
           unquote(body)
         end
--- test/parametric_test_test.exs
+++ test/parametric_test_test.exs
@@ -3,11 +3,17 @@ defmodule ParametricTestTest do
   require ParametricTest
   import ParametricTest
 
+  @params [{1, 2}, {3, 4}]
+
   setup do
     {:ok, %{c: 1}}
   end
 
   parametric_test "a test", {a, b} <- [{1, 2}, {3, 4}, {4, 5}], %{c: c} do
+    assert a + c == b
+  end
+
+  parametric_test "a test using attributes", {a, b} <- @params, %{c: c} do
     assert a + c == b
   end
 end
